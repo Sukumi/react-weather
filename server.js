e24ef1853db172eaea3ea98716170621f16058e5
Stephan Petzchen <citizen4@Stephans-MBP.fritz.box>
2016-10-25 08:57:15 +0200

    Update server.js for localhost

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-//Redirect https to http
+//Redirect https to http (considers also localhost
 app.use(function(req, res, next){
-    if(req.headers['x-forwarded-proto'] === 'http'){
-        next();
-    }else{
+    if(req.headers['x-forwarded-proto'] === 'https'){
         res.redirect('http://' + req.hostname + req.url);
+    }else{
+        next();
     }
 })
 
